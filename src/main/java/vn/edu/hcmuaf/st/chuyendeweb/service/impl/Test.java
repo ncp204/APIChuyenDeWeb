56ddc87360d87f66b7e08d8254d5da87ac7e5136
ncp204 <19130172@st.hcmuaf.edu.vn>
2023-04-27 15:21:35 +0700

    cập nhật token cho api gửi mail

--- src/main/java/vn/edu/hcmuaf/st/chuyendeweb/service/impl/Test.java
+++ src/main/java/vn/edu/hcmuaf/st/chuyendeweb/service/impl/Test.java
@@ -5,6 +5,7 @@ import com.cloudinary.utils.ObjectUtils;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import vn.edu.hcmuaf.st.chuyendeweb.dto.request.LaptopDTO;
+import vn.edu.hcmuaf.st.chuyendeweb.mail.GMailer;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -13,13 +14,18 @@ import java.util.Map;
 
 public class Test {
 
-    public static void main(String[] args) {
-        // Tạo đối tượng Gson
-        Gson gson = new Gson();
-        String jsonString = "{\"battery\":\"Thinkpad x1 carbon gen 10\",\"brand\":\"Thinkpad x1 carbon gen 10\",\"chipCpu\":\"Thinkpad x1 carbon gen 10\",\"color\":\"Thinkpad x1 carbon gen 10\",\"cpu\":\"Thinkpad x1 carbon gen 10\",\"display\":\"Thinkpad x1 carbon gen 10\",\"graphics\":\"Thinkpad x1 carbon gen 10\",\"laptopState\":\"Thinkpad x1 carbon gen 10\",\"price\":\"100\",\"productName\":\"Thinkpad x1 carbon gen 10\",\"quantity\":\"10\",\"ram\":\"Thinkpad x1 carbon gen 10\",\"storage\":\"Thinkpad x1 carbon gen 10\",\"type\":\"Thinkpad x1 carbon gen 10\",\"weight\":\"12\"}";
-        // Chuyển đổi chuỗi thành đối tượng JSON
-        JsonObject jsonObject = gson.fromJson(jsonString, JsonObject.class);
-        LaptopDTO laptopDTO = gson.fromJson(jsonObject, LaptopDTO.class);
-        System.out.println(laptopDTO);
+    public static void main(String[] args) throws Exception {
+//        // Tạo đối tượng Gson
+//        Gson gson = new Gson();
+//        String jsonString = "{\"battery\":\"Thinkpad x1 carbon gen 10\",\"brand\":\"Thinkpad x1 carbon gen 10\",\"chipCpu\":\"Thinkpad x1 carbon gen 10\",\"color\":\"Thinkpad x1 carbon gen 10\",\"cpu\":\"Thinkpad x1 carbon gen 10\",\"display\":\"Thinkpad x1 carbon gen 10\",\"graphics\":\"Thinkpad x1 carbon gen 10\",\"laptopState\":\"Thinkpad x1 carbon gen 10\",\"price\":\"100\",\"productName\":\"Thinkpad x1 carbon gen 10\",\"quantity\":\"10\",\"ram\":\"Thinkpad x1 carbon gen 10\",\"storage\":\"Thinkpad x1 carbon gen 10\",\"type\":\"Thinkpad x1 carbon gen 10\",\"weight\":\"12\"}";
+//        // Chuyển đổi chuỗi thành đối tượng JSON
+//        JsonObject jsonObject = gson.fromJson(jsonString, JsonObject.class);
+//        LaptopDTO laptopDTO = gson.fromJson(jsonObject, LaptopDTO.class);
+
+
+        String email = "19130172@st.hcmuaf.edu.vn";
+        String subject = "test";
+        String body = "Test gui mail";
+        new GMailer().sendMail(email, subject, body);
     }
 }
